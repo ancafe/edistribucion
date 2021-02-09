0705ac18e98bccc9f5fb00fcfd698d434ef591a9
Ángel Cardiel <angel.cardiel@protonmail.com>
2021-02-09 17:01:26 +0100

    forked

---
+++ auto.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 20 11:51:36 2020
+
+@author: trocotronic
+foked by @ancafe
+"""
+import os
+
+from EdistribucionAPI import Edistribucion
+from configparser import ConfigParser
+from datetime import datetime
+import functions
+import mariadb
+import sys
+import json
+
+config_object = ConfigParser()
+carpeta = os.path.dirname(os.path.realpath(__file__))
+file = carpeta + "/config.ini"
+config_object.read(file)
+db = config_object["DATABASE"]
+
+try:
+   conn = mariadb.connect(
+      user=db['user'],
+      password=db['pwd'],
+      host=db['host'],
+      port=int(db['port']),
+      database=db['database'])
+
+except mariadb.Error as e:
+   print(f"Error conectando a la base de datos: {e}")
+   sys.exit(1)
+
+cur = conn.cursor()
+
+query = f"SELECT * FROM login"
+cur.execute(query)
+rows = cur.fetchall()
+
+for login in rows:
+    edis = Edistribucion(login[0], login[1])
+    edis.login()
+    r = edis.get_cups()
+
+    for cup in r['data']['lstCups']:
+        s = edis.get_cups_detail(cup['Id'])
+        nombre = edis.get_login_info()
+        lstATR = s['lstATR'][0]['Id']
+        print("CUP ", cup['Id'], ': ', cup['Direccion'])
+        cups = cup['Id']
+        try:
+            cur.execute(
+                "INSERT IGNORE cups (`cups`, `titular`, `direccion`, `id`, `asr`) VALUES (%s,%s,%s,%s,%s)",
+                (cup['Name'], nombre['Name'], cup['Direccion'], cup['Id'], lstATR))
+        except mariadb.Error as e:
+            print(f"Error: {e}")
+        conn.commit()
+        functions.readConsumo(edis, cur, conn, lstATR, cup['Name'])
+
+    fileAccess = carpeta  + '/edistribucion.access'
+    fileSession = carpeta + '/edistribucion.session'
+    if os.path.exists(fileAccess):
+        os.remove(fileAccess)
+    if os.path.exists(fileSession):
+            os.remove(fileSession)
+
+
+conn.close()
---
+++ functions.py
@@ -0,0 +1,22 @@
+import mariadb
+
+from EdistribucionAPI import Edistribucion
+from datetime import datetime
+
+def readConsumo( edis, cur, conn, cups, real ):
+    # OBTENEMOS CONSUMO
+    print("MEDIMO CONSUMO")
+    print(cups)
+    consumo = edis.get_measure(cups)
+    if 'mapHourlyPoints' in consumo['data']:
+        for el, value in consumo['data']['mapHourlyPoints'].items():
+            for dia in value:
+                date_object = datetime.strptime(el, '%d-%m-%Y').strftime('%Y-%m-%d')
+                try:
+                    cur.execute(
+                        "INSERT INTO consumos (`cups`, `date`, `hour`, `invoiced`, `obtainingMethod`, `real`, `value`) VALUES (%s,%s,%s,%s,%s,%s,%d) ON DUPLICATE KEY UPDATE `value`=%d",
+                        (real, date_object, dia['hour'], dia['invoiced'], dia['obtainingMethod'], dia['real'], dia['value'], dia['value']))
+                except mariadb.Error as e:
+                    print(f"Error: {e}")
+    conn.commit()
+    return
